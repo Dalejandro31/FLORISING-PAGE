2eccb2637708190cb48f988f5077fd03ce886672
Dalejandro31 <ao7417331@gmail.com>
2023-11-01 09:28:43 -0500

    NavBar

---
+++ client/src/components/navBar/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="NavBar">
+        <div class="logo">
+            <p>FLORISING</p>
+        </div>
+        
+        <div class="links">
+            <ul>
+                <li><button href="">INICIO</button></li>
+                <li><button href="">SERVICIOS</button></li>
+                <li><button href="">SOBRE NOSOTROS</button></li>
+                <li><button href="">CONTACTANOS</button></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+<style scoped>
+@import './Nav.css';
+</style>
