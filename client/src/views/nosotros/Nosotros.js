4665fefcf0f44decdcd7f40e740b8c0daf7d71e1
Dalejandro31 <ao7417331@gmail.com>
2023-11-02 22:03:19 -0500

    vista sobre nosotros

---
+++ client/src/views/nosotros/Nosotros.js
@@ -0,0 +1,52 @@
+const content = {};
+
+content.title = `SOBRE NOSOTROS`;
+content.titleMision = `MISION`;
+content.content1 = 
+    `Hemos establecido un centro de carga en frío, con el fin de brindar el
+    servicio de almacenamiento y consolidación, para las diferentes empresas
+    que necesiten ubicar sus productos bajo condiciones controladas de
+    temperatura, de acuerdo a los requerimientos de cada una de las
+    compañías para mantener en óptimas condiciones la calidad de sus
+    productos y la cadena en frío
+    Almacenamos artículos perecederos en cuartos fríos especializados que
+    mantienen los productos en óptimas condiciones. Analizamos la logística de
+    cada orden individual, atendiendo a las necesidades del producto y del
+    cliente, buscando siempre el mejoramiento continuo en la prestación de
+    nuestros servicios.`;
+content.titleVision = `VISION`;
+content.content2 = 
+    `Nuestra infraestructura física y digital está diseñada para
+    ofrecer servicios de alta calidad enfocados en procesos
+    de mejoramiento continuo; Generando cada vez más
+    empleo y brindando un servicio de calidad.
+    Contamos con estándares técnicos de calidad que nos
+    han permitido hacer alianza estratégica con nuestros
+    clientes y ofrecerles en el menor tiempo posible los
+    mejores niveles de satisfacción, evitándoles costos
+    innecesarios y riesgos en los procesos siendo amigables
+    con el medio ambiente.`;
+content.titleReseñaHistorica = `RESEÑA HISTORICA FLORISING CLG`;
+content.content3 = 
+    `Desde 2012 , FLORISING COMMERCIAL & LOGISTIC GROUP SAS, con
+    la nueva razón social ha dirigido sus esfuerzos para garantizar una
+    cadena de frio, esta idea surge de la necesidad de servir inicialmente
+    en el campo de las exportaciones de productos perecederos. Creada
+    por el señor Víctor Raúl Carvajal Cuta donde se asocia con cargo
+    máster y hacen del centro de consolidación uno de los más grandes
+    de la sabana en manejo de marítimos.`;
+content.content4 = 
+    `En el año 2017, se termina la sociedad ,pero siguen funcionando, es
+    así, que logra con más de 10 años en el mercado, consolidar
+    aproximadamente de 15 clientes potenciales, quienes siguen
+    premiando nuestros esfuerzos por entender sus necesidades y
+    satisfacer sus requerimientos respondiendo con resultados óptimos,
+    en tiempo record y con los mejores precios del mercado.`;
+content.tituloObjetivos = `OBJETIVOS DEL SGCS BASC`;
+content.content5 = `Florisig Commercial & Logistics Group S.A.S, en 
+concordancia con su Política de Seguridad, establece los siguientes Objetivos
+como un compromiso con la seguridad en sus operaciones de consolidación de carga.`;
+content.integridadProcesos = `Mantener la integridad en cada uno de los proceso.`;
+
+
+export {content}
