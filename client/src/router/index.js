917455eca2494a30cae003dd64ab2caf445d6bbc
unknown <nicogar1999@gmail.com>
2023-11-06 22:30:36 -0500

    Avance Servicios

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/homePage/HomePage.vue'
 import Nosotros from '../views/nosotros/Nosotros.vue'
+import Servicios from '../views/servicios/Servicios.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomePage
+  },
+  {
+    path: '/servicios',
+    name: 'Servicios',
+    component: Servicios
   },
   {
     path: '/nosotros',
---
+++ client/src/views/servicios/Servicios.js
@@ -0,0 +1,35 @@
+const content ={};
+
+content.title1 = 'RECEPCION Y CONSOLIDACION EN FRIO';
+content.recepcionfrio1 = 
+    `MATERIAL DE EMPAQUE
+    (Estibas, esquineros, zunchos)`
+content.recepcionfrio2 =    
+    `ARMADO DE PALETS
+    (Organizacion y tratamiento adecuado ...)`
+content.recepcionfrio3 =
+    `PRE-COOLING
+    (Ocho (8) unidades de pre-frío para pallets de hasta 
+    2.5Mts de altura)`
+content.recepcionfrio4=
+    `CARGUE EN FRIO CONTENEDOR
+    (Dos (2) muelles de carga con conexión directa al camión 
+    garantizando la cadena de frío)`;
+
+content.title2 = 'ALMACENAMIENTO EN FRÍO'
+content.almacenamientoFrio =
+    `Operaciones de Cross Docking Capacidad
+    de 460Mts`
+content.almacenamientoFrio2 =
+    `Refrigeración hasta -18°C`;
+
+content.title3 = 'TRANSPORTE REFRIGERADO'
+content.transporteRefrigerado =
+    `Furgones tipo turbo, van y contenedor`;
+
+content.title4 = 'OTROS SERVICIOS'
+content.otros = 
+`Centro de negocios /
+Oficinas disponibles`
+export {content}
+  
