6740d1479718bee8b09522ef44b34a7dc376d6d8
Dalejandro31 <ao7417331@gmail.com>
2023-11-29 19:27:08 -0500

    cambios en rutas e icono

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/homePage/HomePage.vue'
 import Nosotros from '../views/nosotros/Nosotros.vue'
 import Servicios from '../views/servicios/Servicios.vue'
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory('/'),
   routes
 })
 
