2a7698e07b85034c0a07bd7b771e13fc6d2f41d7
unknown <nicogar1999@gmail.com>
2024-02-22 15:53:52 -0500

    Actualizacion Productos, imagenes, flores

--- client/src/views/productos/Productos.js
+++ client/src/views/productos/Productos.js
@@ -1,15 +1,17 @@
 const content ={}
 
-content.title1 = 'VERDES'
+content.title1 = 'FOLLAJES'
 content.verde1 = 'RUSCUS'
 content.verde2 = 'HELECHO'
 content.verde3 = 'COCCULUS'
 content.verde4 = 'LILY GRASS'
+content.verde5 = 'EUCALIPTO BABY BLUE'
+content.verde6 = 'GYPSO'
 
 content.title2 = 'VARIEDAD DE ROSAS'
 content.flor1 = 'VENDELA'
 content.flor2 = 'GOLDSTRIKE'
-content.flor3 = 'CONNI'
+content.flor3 = 'CONNIE'
 content.flor4 = 'FREEDOM'
 content.flor5 = 'BRIGHTON'
 content.flor6 = 'ORLANDO'
@@ -24,4 +26,14 @@ content.flor14 = 'PINK FLOYD'
 content.flor15 = 'MONDIAL'
 
 content.title3 = 'BOUQUET'
+
+content.title4 = 'VARIEDADES DE DIVERSIFICACIÓN'
+content.diver1 = 'GIRASOL'
+content.diver2 = 'PROTEAS'
+content.diver3 = 'GERBERA'
+content.diver4 = "CLAVEL"
+content.diver5 = "ASTROMELIA"
+content.diver6 = 'AGAPANTOS'
+content.diver7 = 'BOCA DE DRAGÓN'
+content.diver8 = 'ASTER'
 export {content}
